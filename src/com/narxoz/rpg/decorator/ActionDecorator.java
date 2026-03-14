ff5ee39c087dfeeed363e9601b297baf7ee3e4d2
zhadyra <zhadyra.kurmanova@narxoz.kz>
2026-03-14 17:25:17 +0500

    ActionDecorator

--- src/com/narxoz/rpg/decorator/ActionDecorator.java
+++ src/com/narxoz/rpg/decorator/ActionDecorator.java
@@ -1,5 +1,4 @@
 package com.narxoz.rpg.decorator;
-
 public abstract class ActionDecorator implements AttackAction {
     private final AttackAction wrappedAction;
 
@@ -13,19 +12,16 @@ public abstract class ActionDecorator implements AttackAction {
 
     @Override
     public String getActionName() {
-        // TODO: Delegate to wrapped action, then extend if needed.
         return wrappedAction.getActionName();
     }
 
     @Override
     public int getDamage() {
-        // TODO: Delegate to wrapped action, then extend if needed.
         return wrappedAction.getDamage();
     }
 
     @Override
     public String getEffectSummary() {
-        // TODO: Delegate to wrapped action, then extend if needed.
         return wrappedAction.getEffectSummary();
     }
 }
